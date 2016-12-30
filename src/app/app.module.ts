80d8757793a672ff3e6eafb0683d5d3806084a2a
asotilp <asotilp@asotilp-01.olo.com.local>
2016-12-30 18:00:57 -0500

    commit version 1.0 trabajo
    - dev commit angular2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,33 +8,47 @@ import { MaterialModule } from '@angular/material';
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
 import { HomeComponent } from './home/home.component';
+import { BlogComponent } from './blog/blog.component';
+import { MoviesComponent } from './movies/movies.component';
+import { MusicComponent } from './music/music.component';
+import { GiftsComponent } from './gifts/gifts.component';
+import { ContactusComponent } from './contactus/contactus.component';
 
 const appRoutes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'home', component: HomeComponent },
-    { path: 'main', component: MainComponent },
-//  {
-//    path: 'heroes',
-//    component: HeroListComponent,
-//    data: { title: 'Heroes List' }
-//  },  
-//  { path: '**', component: PageNotFoundComponent }
+    { path: 'blog', component: BlogComponent },
+    { path: 'movies', component: MoviesComponent },
+    { path: 'music', component: MusicComponent },
+    { path: 'gifts', component: GiftsComponent },
+    { path: 'contactus', component: ContactusComponent },
+    //  {
+    //    path: 'heroes',
+    //    component: HeroListComponent,
+    //    data: { title: 'Heroes List' }
+    //  },  
+    { path: '**', component: HomeComponent }
 ];
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    MainComponent,
-    HomeComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule,
-    RouterModule.forRoot(appRoutes),
-    MaterialModule.forRoot()
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        MainComponent,
+        HomeComponent,
+        BlogComponent,
+        MoviesComponent,
+        MusicComponent,
+        GiftsComponent,
+        ContactusComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        RouterModule.forRoot(appRoutes),
+        MaterialModule.forRoot()
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
